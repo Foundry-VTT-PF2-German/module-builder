9141ec261e7d96cf8da79b3e67ddec0a1314fa95
MSAbaddon <66370966+MSAbaddon@users.noreply.github.com>
2023-09-10 22:19:25 +0200

    Use pf2 default mapping

--- src/extract-localization-data/extract-localization-data.js
+++ src/extract-localization-data/extract-localization-data.js
@@ -3,27 +3,22 @@ import { replaceProperties } from "../helper/src/util/utilities.js";
 import { extractPack } from "../helper/src/pack-extractor/pack-extractor.js";
 import { PF2_DEFAULT_MAPPING } from "../helper/src/pack-extractor/constants.js";
 
-// Read config file
-const configFile = JSON.parse(
-    readFileSync("./src/extract-localization-data/extract-localization-config.json", "utf-8")
-);
+const adventurePath = "./adventures";
 
-const CONFIG = { ...configFile };
+const CONFIG = { adventurePath: adventurePath, mappings: PF2_DEFAULT_MAPPING };
 
 // Replace linked mappings and savePaths with actual data
 replaceProperties(CONFIG.mappings, ["subMapping"], CONFIG.mappings);
-// replaceProperties(CONFIG.packs, ["mapping"], CONFIG.mappings);
-// replaceProperties(CONFIG.packs, ["savePath"], CONFIG.filePaths.packs);
 
 // Read available adventure directories
-const adventureDirectories = readdirSync(CONFIG.filePaths.adventures);
+const adventureDirectories = readdirSync(CONFIG.adventurePath);
 adventureDirectories.forEach((adventureDirectory) => {
     const adventurePack = JSON.parse(
-        readFileSync(`${CONFIG.filePaths.adventures}/${adventureDirectory}/${adventureDirectory}.json`)
+        readFileSync(`${CONFIG.adventurePath}/${adventureDirectory}/${adventureDirectory}.json`)
     );
     const extractedPackData = extractPack(adventureDirectory, adventurePack, CONFIG.mappings.adventure);
     writeFileSync(
-        `${CONFIG.filePaths.adventures}/${adventureDirectory}/de.${adventureDirectory}.json`,
+        `${CONFIG.adventurePath}/${adventureDirectory}/en.${adventureDirectory}.json`,
         JSON.stringify(extractedPackData.extractedPack, null, 2)
     );
 });
